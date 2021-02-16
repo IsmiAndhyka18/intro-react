a62b537edc981cbe4ccfbf884e4677615ac1f5f4
Ismi Andhykawati <ismitje@Ismis-MacBook-Air.local>
2021-02-16 13:18:40 +0100

    set the clear button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React,{ useState, useRef} from "react" ;
+import React,{ useState, useRef ,useEffect} from "react" ;
 import './App.css';
 import TodoList from './components/TodoList'
+import { v4 as uuidv4 } from 'uuid';
 
-
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
    const [todos, setTodos] = useState([])
    const todoNameRef = useRef()
 
+   //useEFFECT FOR SAFE THE VALUE  WHEN YOU REFESH VALUE IS STILL THERE
+   useEffect(() => {
+     const storedTodos = JSON.parse(localStorage.getItem
+      (LOCAL_STORAGE_KEY))
+     if (storedTodos) setTodos(storedTodos)
+     
+   }, [])
+
+   useEffect (() => {
+     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify (todos))
+   }, [todos])
+   
+   //checkbox
+   function toggleTodo(id){
+     const newTodos = [...todos]
+     const todo = newTodos.find(todo => todo.id === id)
+     todo.complete = !todo.complete
+     setTodos(newTodos)
+   }
+
    function handleAddTodo(e){
      const name = todoNameRef.current.value
      if(name === '') return
      setTodos(prevTodos => {
-       return [...prevTodos,{ id: 1, name: name, complete:false }]
+       return [...prevTodos,{ id: uuidv4(), name: name, complete:false }]
 
      })
      todoNameRef.current.value =null
 
    }
 
+   function handleClearTodo (){
+     const newTodos= todos.filter(todo => !todo.complete)
+     setTodos(newTodos)
+
+   }
   return (
     <>
-      < TodoList  todos={todos}/>
+      < TodoList  todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type="text"/>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Todo</button>
-      <div>0 left todo</div>
+      <button onClick={handleClearTodo}>Clear Todo</button>
+      <div>{todos.filter(todo=> !todo.complete).length} left todo</div>
 
     </>
   );
