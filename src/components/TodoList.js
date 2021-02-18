2a6e93a6d13b836d66e7c41ec31394b5c8784e13
Ismi Andhykawati <ismitje@Ismis-Air.home>
2021-02-18 11:35:09 +0100

    Making delete function for delete button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-function TodoList({todos}) {
-
-
+function TodoList({todos , setTodos}) {
   
+
+  const deleteTodo = (id) => {
+    const deleteArray = [...todos].filter(todo => todo.id !== id);
+    setTodos(deleteArray);
+    
+  };
+
 
   return (
     <ul>
       {todos.map((todo)=>(
         <div key={todo.id} className="todo">
           <input type="radio" className="checkbox" checked={todo.isCompleted && <span>&#x2714;</span>}/> {todo.name}
-          <div className="delete ">
+          <div className="delete" onClick= {() => deleteTodo (todo.id)}>
             &#128465;
           </div>
         </div>
