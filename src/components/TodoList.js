a294da1ab8010f4e41b218fcd2671bdeb6f434d2
Ismi Andhykawati <ismitje@Ismis-Air.home>
2021-02-18 12:24:57 +0100

    Making complete button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
 function TodoList({todos , setTodos}) {
+
   
 
   const deleteTodo = (id) => {
     const deleteArray = [...todos].filter(todo => todo.id !== id);
     setTodos(deleteArray);
-    
+
   };
 
+  const completeTodo = (id ) => {
+
+    let newArray = [...todos];
+
+       newArray.map((todo) => {
+      if(todo.id == id){
+        todo.complete =! todo.complete
+      }
+    })
+    setTodos(newArray)
+
+  };
+
+
+  
 
   return (
     <ul>
       {todos.map((todo)=>(
         <div key={todo.id} className="todo">
-          <input type="radio" className="checkbox" checked={todo.isCompleted && <span>&#x2714;</span>}/> {todo.name}
+          <input type="radio" className="checkbox" onClick= {() => completeTodo (todo.id)} checked={todo.complete}/> {todo.name}
           <div className="delete" onClick= {() => deleteTodo (todo.id)}>
             &#128465;
+            
           </div>
         </div>
       ))}
