126e20e7d0945d243803ad05fff5a7d93db6e58d
Ismi Andhykawati <ismitje@Ismis-Air.home>
2021-02-18 09:56:12 +0100

    Remove button with onchange enter

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,24 +4,24 @@ import { v4 as uuidv4 } from 'uuid'
 export default function TodoForm({todos , setTodos}) {
   const inputRef = useRef();
 
-  function clickHandler() {
-    const inputElement = inputRef.current;
-    const name = inputRef.current.value
-
-        if (name === "") return
-
-        setTodos([ ...todos, {name: name, complete: false, id: uuidv4()}])
-           
-        inputRef.current.value = null
-
-   
-    console.log(name);
-  }
+  const clickHandler = (e) => {
+    if(e.key === 'Enter'){
+     const name = inputRef.current.value
+     if (name === "") return
+     setTodos([ ...todos, {name: name, complete: false, id: uuidv4()}])
+      inputRef.current.value = null
+    }
+ }
+    const keyEnterHandler = (e) =>{
+    if(e.key === 'Enter'){
+        clickHandler()
+     }
+}
   
     return (
       <div>
-          <input  ref={inputRef}  className="todo-input" type="text" placeholder="Whats need to be done ?" />
-           <button  onClick={clickHandler} className="button" type="submit"  value="Submit">ADD</button>
+          <input  ref={inputRef}  onKeyPress={clickHandler} className="todo-input" type="text" placeholder="Whats need to be done ?" />
+          
       </div>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 function TodoList({todos}) {
 
-  
-  
-  
-
 
   return (
     <ul>
